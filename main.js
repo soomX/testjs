a4fa7c54e0ab3d2aadebb6555bae83290222aff3
soomX <59781791+soomX@users.noreply.github.com>
2020-11-11 13:06:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("test");
+var para = document.createElement("p");
+var node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+var element = document.getElementById("div1");
+var child = document.getElementById("p1");
+element.insertBefore(para, child);
+console.log("finished loading.");
